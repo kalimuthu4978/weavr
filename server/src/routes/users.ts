75d8f4a8a469d888d815bcf286bdbd0e59cd50bd
kalimuthu4978 <kalimuthu4978@gmail.com>
2026-07-18 08:29:23 +0530

    polish: Weavr tab title and favicon

--- server/src/routes/users.ts
+++ server/src/routes/users.ts
@@ -1,5 +1,5 @@
 import express from "express";
-import User from "../models/User";
+import User from "../models/user";
 import requireAuth from "../middleware/auth";
 
 const router = express.Router();
